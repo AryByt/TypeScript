c847640140f21bc47653322f325aa8ec8db0aa32
arybyt <bayatiarya@gmail.com>
2022-06-27 13:42:44 -0500

    testing out React Functional Component React.FC

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -5,3 +5,10 @@ interface ChildProps {
 export const Child = ({color}: ChildProps) => {
   return <div>Testing Child parent { color }</div>;
 };
+
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+  return (
+    <div>{color}</div>
+  )
+}
+  
